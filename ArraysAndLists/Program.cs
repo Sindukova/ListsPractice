de00dd930ebad6439ecf6d6d3c403313dcbd9176
Hanna Sindiukova <Sindukova94@gmail.com>
2020-08-29 13:22:36 -0300

    ListsPractice

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters;
 
 namespace ArraysAndLists
 {
@@ -9,11 +11,38 @@ namespace ArraysAndLists
             //TODO:
 
             // Create an int Array and populate numbers 1-10
+            int num = 1;
+            int [] array1 = new int[10];
+            for(int i=0; i<array1.Length; i++)
+            {
+                array1[i] = num;
+                num++;
 
+            }
+
+            var evens = new List<int>();
+
+
+            var odds = new List<int>();
+
+            for (int i =0; i<50; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    evens.Add(i);
+                }
+                else
+                {
+                    odds.Add(i);
+                }
+            }
             /* Create two Lists of type int.
              * Name one List "evens"
              * Name the other List "odds"
              */
+
+
+
 
             /* Using either a foreach or for loop,
              * nest an if statement to check to see
@@ -27,6 +56,16 @@ namespace ArraysAndLists
              *
              * Try to be creative in your display
              */
+            Console.WriteLine("Even numbers from 0 to 50 are: ");
+            foreach(var i in evens)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Odd numbers from 0 to 50 are: ");
+            foreach (var i in odds)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
